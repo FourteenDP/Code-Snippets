dc3db02289b432784d45187dd8f074c2bbe7a679
FourteenD <2899385645@qq.com>
2023-02-22 15:52:20 +0800

    vault backup: 2023-02-22 15:52:19
    Affected files:

--- 观察者模式.ts
+++ 观察者模式.ts
@@ -1,44 +1,107 @@
-// 观察者模式
-namespace Observer {
-  class Subject {
-    private observers: Observer[] = [];
-    // 添加观察者
-    addObserver(observer: Observer) {
+/**
+ * The Subject interface declares a set of methods for managing subscribers.
+ */
+interface Subject {
+  // Attach an observer to the subject.
+  attach(observer: Observer): void;
+
+  // Detach an observer from the subject.
+  detach(observer: Observer): void;
+
+  // Notify all observers about an event.
+  notify(): void;
+}
+
+/**
+* The Subject owns some important state and notifies observers when the state
+* changes.
+*/
+class ConcreteSubject implements Subject {
+  /**
+   * @type {number} For the sake of simplicity, the Subject's state, essential
+   * to all subscribers, is stored in this variable.
+   */
+  public state: number;
+
+  /**
+   * @type {Observer[]} List of subscribers. In real life, the list of
+   * subscribers can be stored more comprehensively (categorized by event
+   * type, etc.).
+   */
+  private observers: Observer[] = [];
+
+  /**
+   * The subscription management methods.
+   */
+  public attach(observer: Observer): void {
+      const isExist = this.observers.includes(observer);
+      if (isExist) {
+          return console.log('Subject: Observer has been attached already.');
+      }
+
+      console.log('Subject: Attached an observer.');
       this.observers.push(observer);
-    }
-    // 删除观察者
-    removeObserver(observer: Observer) {
-      this.observers = this.observers.filter(
-        (item) => item !== observer
-      );
-    }
-    // 通知观察者
-    notifyObservers(data: any) {
-      this.observers.forEach((observer) => observer.update(data));
-    }
   }
 
-  class Observer {
-    update(data: any) {
-      console.log(data);
-    }
+  public detach(observer: Observer): void {
+      const observerIndex = this.observers.indexOf(observer);
+      if (observerIndex === -1) {
+          return console.log('Subject: Nonexistent observer.');
+      }
+
+      this.observers.splice(observerIndex, 1);
+      console.log('Subject: Detached an observer.');
   }
 
-  // 测试
-  const subject = new Subject();
+  /**
+   * Trigger an update in each subscriber.
+   */
+  public notify(): void {
+      console.log('Subject: Notifying observers...');
+      for (const observer of this.observers) {
+          observer.update(this);
+      }
+  }
 
-  const observer1 = new Observer();
-  const observer2 = new Observer();
-  const observer3 = new Observer();
+  /**
+   * Usually, the subscription logic is only a fraction of what a Subject can
+   * really do. Subjects commonly hold some important business logic, that
+   * triggers a notification method whenever something important is about to
+   * happen (or after it).
+   */
+  public someBusinessLogic(): void {
+      console.log('\nSubject: I\'m doing something important.');
+      this.state = Math.floor(Math.random() * (10 + 1));
 
-  subject.addObserver(observer1);
-  subject.addObserver(observer2);
-  subject.addObserver(observer3);
+      console.log(`Subject: My state has just changed to: ${this.state}`);
+      this.notify();
+  }
+}
 
-  subject.notifyObservers('hello world');
+/**
+* The Observer interface declares the update method, used by subjects.
+*/
+interface Observer {
+  // Receive update from subject.
+  update(subject: Subject): void;
+}
 
-  subject.removeObserver(observer2);
+/**
+* Concrete Observers react to the updates issued by the Subject they had been
+* attached to.
+*/
+class ConcreteObserverA implements Observer {
+  public update(subject: Subject): void {
+      if (subject instanceof ConcreteSubject && subject.state < 3) {
+          console.log('ConcreteObserverA: Reacted to the event.');
+      }
+  }
+}
 
-  subject.notifyObservers('hello world');
-
+class ConcreteObserverB implements Observer {
+  public update(subject: Subject): void {
+      if (subject instanceof ConcreteSubject && (subject.state === 0 || subject.state >= 2)) {
+          console.log('ConcreteObserverB: Reacted to the event.');
+      }
+  }
 }
