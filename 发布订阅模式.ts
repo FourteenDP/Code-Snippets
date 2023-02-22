15bdd85584530230627a9d84ef26cf39ea164c58
FourteenD <2899385645@qq.com>
2023-02-22 13:19:58 +0800

    vault backup: 2023-02-22 13:19:56
    Affected files:

--- 发布订阅模式.ts
+++ 发布订阅模式.ts
@@ -1,29 +1,45 @@
-// 发布订阅模式
 namespace PubSub {
   class PubSub {
-    private events: any;
-    constructor() {
-      this.events = {};
-    }
-    on(event: string, callback: Function) {
-      if (!this.events[event]) {
-        this.events[event] = [];
+    private static instance: PubSub;
+    private constructor() { }
+    static getInstance(): PubSub {
+      if (!PubSub.instance) {
+        PubSub.instance = new PubSub();
       }
-      this.events[event].push(callback);
+      return PubSub.instance;
     }
-    emit(event: string, ...args: any[]) {
-      if (this.events[event]) {
-        this.events[event].forEach((callback: Function) => {
-          callback(...args);
-        });
+
+    private subscribers: { [key: string]: Function[] } = {};
+
+    subscribe(event: string, callback: Function) {
+      if (!this.subscribers[event]) {
+        this.subscribers[event] = [];
       }
+      this.subscribers[event].push(callback);
     }
-    off(event: string, callback: Function) {
-      if (this.events[event]) {
-        this.events[event] = this.events[event].filter(
-          (cb: Function) => cb !== callback
-        );
+
+    unsubscribe(event: string, callback: Function) {
+      if (!this.subscribers[event]) {
+        return;
       }
+      this.subscribers[event] = this.subscribers[event].filter(
+        (subscriber) => subscriber !== callback
+      );
+    }
+
+    publish(event: string, data: any) {
+      if (!this.subscribers[event]) {
+        return;
+      }
+      this.subscribers[event].forEach((subscriber) => subscriber(data));
     }
   }
+
+  // 测试
+  const pubSub = PubSub.getInstance();
+  const callback = (data: any) => console.log(data);
+  pubSub.subscribe("test", callback);
+  pubSub.publish("test", "hello world");
+  pubSub.unsubscribe("test", callback);
+  pubSub.publish("test", "hello world");
 }
