27c0ed2f03e61ba33c5cf3c1bc44c3c8dc31b22b
FourteenD <2899385645@qq.com>
2023-03-11 23:32:26 +0800

    vault backup: 2023-03-11 23:32:20
    Affected files:

--- 聚合函数(compose).ts
+++ 聚合函数(compose).ts
@@ -1,5 +1,8 @@
 namespace CodeSnippets {
   export function compose(...fns: Function[]) {
+    if (fns.length === 0) {
+      return (arg: any) => arg;
+    }
     return function (arg: any) {
       return fns.reduceRight((result, fn) => fn(result), arg);
     };
